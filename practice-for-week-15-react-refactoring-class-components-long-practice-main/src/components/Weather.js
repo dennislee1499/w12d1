93a6ea4053a6d7ff22f4eee6e78853aab102e0c2
Dennis Lee <dennislee1499@gmail.com>
2023-09-11 14:37:14 -0400

    finished phase 3

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Weather.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { toQueryString } from '../utils';
 
-class Weather extends React.Component {
+class OldWeather extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -29,7 +29,7 @@ class Weather extends React.Component {
       "process.env.<variable_name>". Make sure to .gitignore your .env file!
       Also remember to restart your server (i.e., re-run "npm start") whenever
       you change your .env file. */
-      const apiKey = '???';
+      const apiKey = process.env.REACT_APP_WEATHER_API;
 
       const params = {
         lat: location.coords.latitude,
@@ -81,4 +81,73 @@ class Weather extends React.Component {
   }
 }
 
+
+function Weather(props, state) {
+  const [weather, setWeather] = useState(null);
+
+  const pollWeather = async (location) => {
+    let url = "http://api.openweathermap.org/data/2.5/weather?";
+
+    const apiKey = process.env.REACT_APP_WEATHER_API;
+
+    const params = {
+      lat: location.coords.latitude,
+      lon: location.coords.longitude,
+      appid: apiKey,
+    };
+
+    url += toQueryString(params);
+
+    const res = await fetch(url);
+    if (res.ok) {
+      const weather = await res.json();
+      setWeather(weather);
+    } else {
+      alert("Check Weather API key!");
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation?.getCurrentPosition(
+      pollWeather,
+      (err) => console.log(err),
+      { timeout: 10000 }
+    ); 
+  }, []);
+  
+  
+  
+  let content = <div className='loading'>loading weather...</div>;
+  
+  if (weather) {
+    const temp = (weather.main.temp - 273.15) * 1.8 + 32;
+    content = (
+      <div>
+        <p>{weather.name}</p>
+        <p>{temp.toFixed(1)} degrees</p>
+      </div>
+    );
+  }
+  else {
+    content = (
+      <div>
+        Weather is currently unavailable. (Are Location Services enabled?) 
+      </div>
+    )
+  }
+
+  return (
+    <section className="weather-section">
+      <h1>Weather</h1>
+      <div className='weather'>
+        {content}
+      </div>
+    </section>
+  );
+  }
+
+
+
+
 export default Weather;
+
