7240dca84bb170d74f0b540b500ffcb2f447ac3e
Foris8 <foristang@gmail.com>
2023-09-11 15:26:59 -0400

    finish phase 4

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Clock.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/components/Clock.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export class ClockToggle extends React.Component {
-  render () {
-    return (
-      <button 
+// export class ClockToggle extends React.Component {
+//   render () {
+//     return (
+//       <button 
+//         type="button"
+//         className="clock-toggle" 
+//         onClick={this.props.toggleClock}
+//       >
+//         Toggle Clock
+//       </button>
+//     )
+//   }
+// } 
+
+export function ClockToggle(props){
+  return(
+      <button
         type="button"
         className="clock-toggle" 
-        onClick={this.props.toggleClock}
+        onClick={props.toggleClock}
       >
         Toggle Clock
       </button>
     )
-  }
-} 
+}
 
-class Clock extends React.Component {
+
+
+class OldClock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -73,5 +87,63 @@ class Clock extends React.Component {
     );
   }
 }
+
+
+function Clock(){
+  const [time,setTime] = useState(new Date());
+  
+  const tick = () => {
+    return setTime(new Date());
+  }
+
+  useEffect(()=> {
+    const interval = setInterval(tick,1000);
+    
+    // return (
+    //   clearInterval(interval)
+    // )
+    
+  },[])
+
+  
+
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  hours = (hours < 10) ? `0${hours}` : hours;
+  minutes = (minutes < 10) ? `0${minutes}` : minutes;
+  seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+  const timezone = time
+    .toTimeString() // Form: "14:39:07 GMT-0600 (PDT)"
+    .replace(/[^A-Z]/g, "") // Strip out all but capitals
+    .slice(3); // Eliminate initial GMT
+
+  return (
+    <section className="clock-section">
+      <h1>Clock</h1>
+      <div className='clock'>
+        <p className="time">
+          <span>
+            Time:
+          </span>
+          <span>
+            {`${hours}:${minutes}:${seconds} ${timezone}`}
+          </span>
+        </p>
+        <p className="date">
+          <span>
+            Date:
+          </span>
+          <span>
+            {time.toDateString()}
+          </span>
+        </p>
+      </div>
+    </section>
+  );
+}
+
 
 export default Clock;
